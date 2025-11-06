076faccb4ec13873484006501cef4db4a588ecb3
Reinis Janovskis <reinis.janovskis@hotmail.com>
2025-11-06 21:27:40 +0000

    Add acceptance tests

--- frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
+++ frontend/cypress/e2e/1-Browse-Hangry-Hippo/spec.cy.js
@@ -1,35 +1,48 @@
-describe('Hits Home Page', () => {
-  it('passes', () => {
-    cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/');
+describe("Hits Home Page", () => {
+  it("passes", () => {
+    cy.visit(
+      "http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/"
+    );
   });
-  it('displays the Welcome message', () => {
+  it("displays the Welcome message", () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     // Then, we use `should` to assert that there are is a Welcome message.
-    cy.get('h1').should(
-      'have.text',
+    cy.visit(
+      "http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/"
+    );
+    cy.get("h1").should(
+      "have.text",
       "Welcome to Hangry Hippo! Hungry? Let's get started!"
     );
   });
-  it('shows the Menu items', () => {
+  it("shows the Menu items", () => {
     //Check for the Appeteasers menu item
-    cy.get('[data-testid=category-item]')
-      .should('be.visible')
-      .and('contain', 'Appeteasers');
+    cy.visit(
+      "http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/"
+    );
+    cy.get("[data-testid=category-item]")
+      .should("be.visible")
+      .and("contain", "Appeteasers");
 
     //Check for the Handhelds menu item
-    cy.get('[data-testid=category-item]')
-      .should('be.visible')
-      .and('contain', 'Handhelds');
+    cy.get("[data-testid=category-item]")
+      .should("be.visible")
+      .and("contain", "Handhelds");
   });
 });
 
-describe('Hits Order Page', () => {
-  it('passes', () => {
-    cy.visit('http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/order');
+describe("Hits Order Page", () => {
+  it("passes", () => {
+    cy.visit(
+      "http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/order"
+    );
   });
-  it('shows Your Order title', () => {
+  it("shows Your Order title", () => {
     // We use the `cy.get()` command to get all elements that match the selector.
     //Check to see if 'Your Order' exists
-    cy.get('h3').should('have.text', 'Your Order');
+    cy.visit(
+      "http://hangryweb-env.eba-vdyerazm.us-east-1.elasticbeanstalk.com/order"
+    );
+    cy.get("h3").should("have.text", "Your Order");
   });
 });
